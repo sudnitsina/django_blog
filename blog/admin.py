25b717a84dd86a0ff39924cc0a9e0b45534c6798
anya <sudnitsina@gmail.com>
2018-06-26 21:38:03 +0300

    added filters to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'created_date', 'published_date', 'tag_list')
+    list_filter = ('published_date', 'tags')
+    search_fields = ('title',)
+
+    def get_queryset(self, request):
+        return super(PostAdmin, self).get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return format_html(", ".join(
+            "<a href='?tags__id__exact={}'>{}</a>".format(o.id, o.name) for o in obj.tags.all()))
