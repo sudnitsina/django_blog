f1f19bcf70b91fcc42651b38ff0340b7daa3967b
anya <sudnitsina@gmail.com>
2018-06-28 11:55:02 +0300

    set secret key as environment variable

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9!!fctlci7%w)_nl54493f3*2g!!%b#@zeo13%065^!a(e@#9q'
+SECRET_KEY=os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
