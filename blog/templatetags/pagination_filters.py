40f2188b7dda0b926402f36c042e7c835f7fdf1f
anya <sudnitsina@gmail.com>
2019-02-18 02:00:41 +0300

    add docstring to pagination filter

--- blog/templatetags/pagination_filters.py
+++ blog/templatetags/pagination_filters.py
@@ -5,6 +5,11 @@ register = template.Library()
 
 @register.filter
 def truncate(paginator, number):
+    """
+    :param paginator: paginator.num_pages
+    :param number: current page number
+    :return: pages range to show
+    """
     if number < 5:
         truncated_paginator = range(1, min(paginator+1, number+3))
     elif 4 < number < (paginator-3):
