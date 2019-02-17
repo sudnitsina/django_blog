68d89c7f142668a947658ab8223b5f8181fa776b
anya <sudnitsina@gmail.com>
2019-02-18 02:06:17 +0300

    add logging and change verifications in tests.py

--- blog/tests/tests.py
+++ blog/tests/tests.py
@@ -1,3 +1,6 @@
+import logging
+
+from django.core.paginator import Paginator
 from django.contrib.auth.models import User
 from django.template import Template, Context
 from django.test import TestCase
@@ -5,11 +8,18 @@ from django.urls import reverse
 
 from blog.models import Post
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 
 class BlogTest(TestCase):
     fixtures = ["blog/tests/test_data.json"]
 
+    def setUp(self):
+        logger.info(f"'{self._testMethodName}' testcase started")
+
     def test_list(self):
+
         url = reverse('blog:post_list')
         res = self.client.get(url)
         self.assertEquals(res.status_code, 200)
@@ -18,12 +28,15 @@ class BlogTest(TestCase):
 
     def test_create_post(self):
         # create new post
+        self.assertFalse(Post.objects.filter(slug='posting-test').exists())
         self.client.force_login(User.objects.get_or_create(username='tester')[0])
         url = reverse('blog:post_new')
         res = self.client.post(url, {'title': 'Posting test', 'text': "Some text"})
         self.assertRedirects(res, '/post/posting-test/')
 
         # check created post
+        self.assertTrue(Post.objects.filter(slug='posting-test').exists())
+
         url = reverse('blog:post_detail', kwargs={"slug": 'posting-test'})
         res = self.client.get(url)
         self.assertEquals(res.status_code, 200)
@@ -37,22 +50,17 @@ class BlogTest(TestCase):
         # print(res.content.decode())
         self.assertRedirects(res, '/admin/login/?next=/new/')
 
-        # check post is not created
-        url = reverse('blog:post_detail', kwargs={"slug": 'posting-test'})
-        res = self.client.get(url)
-        self.assertEquals(res.status_code, 404)
+        self.assertFalse(Post.objects.filter(title='Posting test').exists())
 
     def test_delete_post(self):
+        self.assertTrue(Post.objects.filter(slug='testing-tools').exists())
         # delete post
         self.client.force_login(User.objects.get_or_create(username='tester')[0])
         url = reverse('blog:post_detail', kwargs={"slug": 'testing-tools'})
         res = self.client.post(url, {'action': 'delete'})
         self.assertRedirects(res, '/')
 
-        # check post is deleted
-        url = reverse('blog:post_detail', kwargs={"slug": 'testing-tools'})
-        res = self.client.get(url)
-        self.assertEquals(res.status_code, 404)
+        self.assertFalse(Post.objects.filter(slug='testing-tools').exists())
 
     def test_delete_post_unauthorised(self):
         # delete post without authorisation
@@ -89,3 +97,18 @@ class SidePanelTest(TestCase):
         rendered = self.TEMPLATE.render(Context({}))
         self.assertIn("Post #5", rendered)
         self.assertNotIn("Post #6", rendered)
+
+
+class PaginatorTest(TestCase):
+    fixtures = ["blog/tests/test_data.json"]
+
+    def test_one_page(self):
+        list_ = Post.objects.all()
+        p = Paginator(list_, 5)
+        posts = p.page(1)
+        self.assertEqual(len(posts), 2)
+
+        p = Paginator(list_, 1)
+        posts = p.page(1)
+        self.assertEqual(len(posts), 1)
+        logging.info(p.page_range)
